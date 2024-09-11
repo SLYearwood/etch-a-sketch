1986791d03544e769b50999dc3c8fe5f69a6c837
SLYearwood <slyearwood7@gmail.com>
2024-09-11 00:59:26 -0400

    removed hardcoded css and added grid size changing

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 createGrid();
 
-function createGrid () {
+function createGrid (gridSize = 16) {
   const container = document.querySelector(".container");
-    for (let i = 0; i < 16; i++) {
+  container.innerHTML = '';
+  const squareSize = 1000 / gridSize;
+    for (let i = 0; i < gridSize * gridSize; i++) {
       const div = document.createElement("div");
-      div.classList.add("row")
+      div.classList.add("column")
+      div.style.height = `${squareSize}px`;
+      div.style.width = `${squareSize}px`;
       container.appendChild(div);
     }
-    const row = document.querySelectorAll('.row');
-    row.forEach((value) => {
-      for (let i = 0; i < 16; i++) {
-        const div = document.createElement("div");
-        div.classList.add("column")
-        value.appendChild(div);
-      }
-      
-    })
-};
-
-
-const squares = document.querySelectorAll(".column");
-squares.forEach((value) => {
-  value.addEventListener("mouseenter", () => {
+  
+    const squares = document.querySelectorAll(".column");
+    squares.forEach((value) => {
+    value.addEventListener("mouseenter", () => {
     value.classList.add("hovering");
   })
-  value.addEventListener("mouseleave", () => {
-    value.classList.remove("hovering");
-  })
 })
+};
+
+function changeGridSize () {
+  let newGridSize = prompt("How many squares would you like? Max 100!")
+  while (newGridSize > 100 || newGridSize < 1 || isNaN(newGridSize)) {
+    if (newGridSize === null) return;
+    newGridSize = prompt("Max grid size exceeded, enter a new number. Max 100!")
+  }
+  const container = document.querySelector(".container");
+  
+  createGrid(Number(newGridSize));
+}
+
+const changeButton = document.querySelector(".change-button");
+changeButton.addEventListener("click", changeGridSize);
+
+
 
 
 
